294fac49d160424eea5c10f5d800495ef4be6384
Mirko Lauff <mirkolauff@gmail.com>
2019-01-20 17:46:36 +0100

    add

--- desc_doap/use_doap.py
+++ desc_doap/use_doap.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Any
+from typing import List, Tuple, Any, Dict
 import cv2
 import numpy as np
 import sys
@@ -8,6 +8,7 @@ import io_utils
 import subprocess
 import shutil
 from tqdm import tqdm
+import pickle
 
 def create_patches(img: np.array, kpts: List[cv2.KeyPoint], N: int) -> List[np.array]:
     """Creats a csv file containing patches around the keypoints given in `kpts`.
@@ -26,105 +27,71 @@ def create_patches(img: np.array, kpts: List[cv2.KeyPoint], N: int) -> List[np.a
 
     return patches
 
+def compute(image_file_path:str, config:Dict, model:Any) -> np.array:
+    """Computes descriptors from keypoints saved in a file."""
 
+    img = cv2.imread(image_file_path, 0)
+    img = io_utils.smart_scale(img, config['max_size'], prevent_upscaling=True) if config['max_size'] is not None else img
 
-def compute(
-    detector: Any,
-    model: Any,
-    image: str,
-    size: int=None) -> Tuple[List[cv2.KeyPoint], np.array, np.array, np.array]:
-    """Computes the keypoints and descriptors for a given input image.
-    Draws keypoints into the image.
-    Returns keypoints, descriptors and image with keypoints.
+     # Infer the path to the corresponding csv file for the keypoints.
+    collection_name, set_name, image_name, _ = io_utils.get_path_components(image_file_path)
 
-    Arguments:
-        detector {Any} -- A keypoint detector.
-        model {None} -- A model to load in python.
-        image {np.array} -- Path to the image.
-        size {None} -- Maximal dimension of image. Default: None.
+    # find path to keypoints file
+    keypoints_file_path = io_utils.build_output_path(
+        config['output_dir'],
+        collection_name,
+        set_name,
+        'keypoints',
+        config['detector_name'],
+        image_name,
+        config['max_size'])
 
-    Returns:
-        Tuple[List[cv2.KeyPoint], np.array, np.array, None] -- Returns tuple (keypoints, descriptors, image with keypoints, image of heatmap).
-    """
-    """Wrapper function for DOAP.
-    1) Create a temporay folder `tmp` to save intermediate output.
-    2a) Load and smart scale the image.
-    2b) Find keypoints inside that image.
-    2c) For each keypoint get a 42x42 patch around that keypoint and save that
-        in the corresponding directory in the `tmp` dir.
-    3) Call now use_doap_with_file.m for the `pathes` file in the `tmp`
-        directory and compute the descriptors.
-    4) Draw list of cv2.KeyPoints into image.
-    5) Return KeyPoint list, descriptors and image with KeyPoints.
-    """
-    base_path, _ =  os.path.splitext(os.path.abspath(__file__))
-    base_path = os.sep.join(base_path.split(os.sep)[:-1])
+    # Load keypoints from csv file as numpy array.
+    kpts_numpy = io_utils.get_keypoints_from_csv(keypoints_file_path)
 
-    # 1)
-    path_tmp = os.path.join(base_path, 'tmp')
-    if not os.path.exists(path_tmp):
-        os.makedirs(path_tmp, exist_ok=True)
+    # Convert numpy array to List of cv2.KeyPoint list
+    kpts_cv2 = io_utils.numpy_to_cv2_kp(kpts_numpy)
 
-    # 2a)
-    img = cv2.imread(image, 0)
-    img = io_utils.smart_scale(img, size, prevent_upscaling=True) if size is not None else img
+    # Create iamge patches for each keypoint
+    patches = create_patches(img, kpts_cv2, 42)
 
-    # 2b)
-    kp, _ = detector.detectAndCompute(img, None)
-
-    # 2c)
-    patches = create_patches(img, kp, 42) # list of patches
-    path_to_patches = os.path.join(path_tmp, 'patches.csv')
-    path_to_desc = os.path.join(path_tmp, 'descriptors.csv')
+    # Save patches in tmp dir
+    path_to_desc = os.path.join(config['tmp_dir_doap'], 'descriptors.csv')
+    path_to_patches = os.path.join(config['tmp_dir_doap'], 'patches.csv')
     io_utils.save_patches_list(patches, path_to_patches)
 
-    # 3)
-    # TODO: file paths for vlfeat, matconvnet and the model must be paramters
+    # Compute descritpors in matlab. Save result in tmp_dir
+    # TODO: file paths for vlfeat, matconvnet and the model must be parameters
     subprocess.check_call(['matlab', '-nosplash', '-r',
     "use_doap_with_file('vlfeat-0.9.21', 'matconvnet-1.0-beta25', 'HPatches_ST_LM_128d.mat', '.', '{}', '{}');quit".format(path_to_patches, path_to_desc)])
 
+    # Load matlab results and return.
     desc = np.loadtxt(path_to_desc, delimiter=',')
 
-    # 4)
-    img_kp = cv2.drawKeypoints(img, kp, None)
+    return desc
 
-    # 5)
-    return (kp, desc, img_kp, None)
-
-
-def main(argv: Tuple[str, str,str]) -> None:
-    """Runs the DOAP model and saves the results.
+def main(argv: Tuple[str]) -> None:
+    """Runs the TILDE model and saves the results.
 
     Arguments:
-        argv {Tuple[str, str, str]} -- List of parameters. Expects exactly three
-            parameters. The first one contains json-fied network information,
-            the second contains the json-fied config object and the third is
-            the json-fied file list with all files to be processed.
+        argv {Tuple[str]} -- List of one parameters. There should be exactly
+            one parameter - the path to the config file inside the tmp dir.
+            This config file will be used to get all other information and
+            process the correct images.
     """
+    if len(argv) <= 0:
+        raise RuntimeError("Missing argument <path_to_config_file>. Abort")
 
-    project_name = 'doap'
-    detector_name = 'SIFT'
-    descriptor_name = 'DOAP'
+    with open(argv[0], 'rb') as src:
+        config_file = pickle.load(src, encoding='utf-8')
 
-    network = json.loads(argv[0])
-    config = json.loads(argv[1])
-    file_list = json.loads(argv[2])
-    detector = cv2.xfeatures2d.SIFT_create()
+    config, file_list = config_file
     model = None
 
-    for file in tqdm(file_list):
-        io_utils.save_output(
-            file,
-            compute(detector, None, file, config['size']),
-            config['output_dir'],
-            detector_name,
-            descriptor_name,
-            project_name,
-            config['size'])
-
-    # Clean up
-    if network['tmp_dir']:
-        shutil.rmtree(network['tmp_dir'], ignore_errors=True)
+    if config['task'] == 'descriptors':
+        for file in tqdm(file_list):
+            descriptors = compute(file, config, model)
+            io_utils.save_descriptor_output(file, config, descriptors)
 
 if __name__ == '__main__':
     argv = sys.argv[1:]
