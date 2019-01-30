665761afb864f2b5ad1a9de8a952c46be72a734f
Mirko Lauff <mirkolauff@gmail.com>
2019-01-30 03:13:27 +0100

    fix: Remove dead code.

--- evaluation/detectors/run_eval.py
+++ evaluation/detectors/run_eval.py
@@ -11,21 +11,6 @@ import util_functions as util
 import eval_functions as efunc
 from Evaluater import Evaluater
 
-def eval__num_kpts(ev:Evaluater) -> pd.DataFrame:
-    file_list = ev.file_list
-
-    output = pd.DataFrame(
-        data=np.zeros((len(file_list), 1), dtype='int32'),
-        index=file_list)
-
-    # Open each file and number of rows, which is equal to number keypoints
-    # that have been found. Insert number into output dataframe.
-    for file_path in file_list:
-        df = pd.read_csv(file_path, sep=',', delimiter='#')
-        output.loc[file_path][0] = df.shape[0]
-
-    return output
-
 def build_list_of_evaluations(config:Dict, file_system:Dict) -> None:
     list_of_evaluations = []
 
