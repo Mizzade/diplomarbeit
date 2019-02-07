e052b7fa445c602437eeabb3860924c68cf8efb3
Mirko Lauff <mirkolauff@gmail.com>
2019-02-07 01:03:10 +0100

    wip: Finish detect_bulk function.

--- det_tcovdet/use_tcovdet.py
+++ det_tcovdet/use_tcovdet.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 import io_utils
 import pickle
 import subprocess
-from scipy.io import savemat
+from scipy.io import savemat, loadmat
 
 def detect_bulk(
     file_list: List[str],
@@ -27,38 +27,28 @@ def detect_bulk(
         the `config` object.
     """
 
-    print('Start trying')
-    # try:
-    #     # Create feature map for each image in 'covariant_points' folder
-    #     subprocess.check_call(['python', 'patch_network_point_test.py',
-    #     '--save_feature', 'covariant_points',
-    #     '--output_dir', config['tmp_dir_tcovdet'],
-    #     '--file_list', ' '.join(file_list),
-    #     ])
+    try:
+        # Create feature map for each image in 'covariant_points' folder
+        subprocess.check_call(['python', 'patch_network_point_test.py',
+        '--save_feature', 'covariant_points',
+        '--output_dir', config['tmp_dir_tcovdet'],
+        '--file_list', ' '.join(file_list),
+        ])
 
-    # except Exception as e:
-    #     print('Something went terrible wrong.')
-    #     print(e)
+    except Exception as e:
+        print('TCovDet: Covariant feature map creation failed.')
+        print(e)
+        raise e
 
     try:
-        # Create .mat files containing 'keypoints'.
-        #dataset_name='webcam'
-        #conv_feature_name='covariant_point_tilde'
-        #feature_name='feature_point_tilde'
-        #network_name='mexico_tilde_p24_Mexico_train_point_translation_iter_20'
-        #stats_name='mexico_tilde_p24_Mexico_train_point'
-
-        # point_number='1000'
-        #$matlab -r "point_extractor('$dataset_name','$conv_feature_name','$feature_name',$point_number);";
-
         collection_names = []
         set_names = []
-        image_bases = []
+        image_names = []
         for file_path in file_list:
             collection_name, set_name, file_base, extension = io_utils.get_path_components(file_path)
             collection_names.append(collection_name)
             set_names.append(set_name)
-            image_bases.append(file_base)
+            image_names.append(file_base)
 
         # The path to this file
         matlab_config_path = os.path.join(config['tmp_dir_tcovdet'], 'filelist.mat')
@@ -73,25 +63,37 @@ def detect_bulk(
             'file_list': file_list,
             'collection_names': collection_names,
             'set_names': set_names,
-            'image_bases': image_base,
+            'image_names': image_names,
             'dir_output': dir_output,
             'dir_data': dir_data,
             'point_number': 1000
         })
         subprocess.check_call(['matlab', '-nosplash', '-r',
-        "point_extractor('vlfeat-0.9.21', '{}')".format(matlab_config_path)])
+        "point_extractor('vlfeat-0.9.21', '{}');quit".format(matlab_config_path)])
 
-        # subprocess.check_call(['matlab', '-nosplash', '-r',
-        # "test_program('vlfeat-0.9.21', 'matconvnet-1.0-beta25', 'HPatches_ST_LM_128d.mat', '.', '{}', '{}');quit".format(path_to_patches, path_to_desc)])
     except Exception as e:
-        print('oopsie.')
+        print('TCovDet: Keypoint feature map creation failed.')
         print(e)
+        raise e
 
+    # Load each created .mat file, extract the keypoints (Column 2 and 5),
+    # create list of cv2.KeyPoint.
+    # Then load the image, scale it, draw the keypoints in it and save everything
+    for i, file in enumerate(file_list):
+        mat_path = os.path.join(config['tmp_dir_tcovdet'], 'feature_points', collection_names[i], set_names[i], image_names[i] + '.mat')
+        kpts_numpy = loadmat(mat_path)['feature'][:, [2, 5]]        # numpy array
+        kpts_cv2 = [cv2.KeyPoint(x[0], x[1]) for x in kpts_numpy]   # list of cv2.KeyPoint
 
+        img = cv2.imread(file, 0)
+        if (img.shape[0] * img.shape[1]) > (1024 * 768):
+            ratio = (1024 * 768 / float(img.shape[0] * img.shape[1]))**(0.5)
+            img = cv2.resize(img, (int(img.shape[1] * ratio), int(img.shape[0] * ratio)), interpolation = cv2.INTER_CUBIC)
 
+        img_kp = cv2.drawKeypoints(img, kpts_cv2, None)
 
-
-
+        # Save everything.
+        io_utils.save_detector_output(file, config['detector_name'], config,
+            kpts_cv2, img_kp, None)
 
 def main(argv: Tuple[str]) -> None:
     """Runs the TCovDet model and saves the results.
@@ -112,13 +114,9 @@ def main(argv: Tuple[str]) -> None:
 
     if config['bulk_mode_tcovdet']:
         detect_bulk(file_list, config)
-        # TODO Save intermediate output as .csv in global output folder
     else:
         raise NotImplemented
-        # for file in tqdm(file_list):
-        #     keypoints, keypoints_image, heatmap_image = detect(file, config)
-        #     io_utils.save_detector_output(file, config['detector_name'], config, keypoints,
-        #         keypoints_image, heatmap_image)
+
 
 if __name__ == "__main__":
     argv = sys.argv[1:]
